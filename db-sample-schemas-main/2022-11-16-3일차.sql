214d4eb1f1f0aee71e9c0ac07c48f1bbebaf6c8e
dongsoju <cnrehdzj@naver.com>
2022-11-16 17:47:10 +0900

    first commit

---
+++ db-sample-schemas-main/2022-11-16-3일차.sql
@@ -0,0 +1,62 @@
+/*----------------
+
+--------------------
+1) 컬럼, 숫자, 문자
+2) 산술연산자 : +, -, *, /, 비교연산자 : =, >=, <=, <, >, ?=, <>,
+3)AND,OR,NOT : 논리연산자
+4)LIKE, IN ,BETWEEN,EXISTS,NOT
+5)IS NULL, IS NOT NULL
+6)ANY, SOME, ALL
+7)함수(어떤 작업을 수행하는 명령어의 단위) (VS프로시저)
+*/    
+    
+2-3 연산자
+2.3.1 산술연산자 : +, - *, /
+SELECT 2+2,2-1,2*3,4/2
+FROM dual;
+
+80번 부서 사원의 한 해 동안 받은 급여(연봉)을 조회하시오
+--사원들의 정보는 EMPLOYEES라는 테이블에 저장되어 있음.
+--사원이 근무하는 부서의 정보는 DEPARTMENTS라는 테이블에 저장되어 있음
+
+
+SELECT   employee_id, last_name, salary * 12 "Annual slary"
+FROM employees
+WHERE department_id=80;
+
+--SELECT department_id, department_name, manager_id
+--FROM departments
+--WHERE department_id = 80;
+
+전체 사원들 중 한해동안 받은 급여가 12000인 사원을 조회하시오
+--전체 사원을 조회 --*AESRERISK,만능문자/모든 문자열을 대체 모든컬럼 뜻
+SELECT  *
+FROM    employees
+WHERE   salary*12 =120000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ study oracle/2022-11-14-1일차.sql
@@ -0,0 +1,105 @@
+SELECT * FROM countries;
+
+SELECT country_id country_name, region_id
+FROM countries;
+
+
+SELECT country_id country_name
+FROM countries;
+
+DESCRIBE employees;
+SELECT *
+FROM employees;  프롬절에 조회하고자하는걸 검색
+
+
+DESC employees;
+
+
+
+EMPLOYEE_ID       NOT NULL
+FIRST_NAME           NOT NULL
+LAST_NAME        NOT NULL
+EMAIL 
+PHONE_NUMBER
+HIRE_DATE           NOT NULL
+JOB_ID               NOT NULL
+SALARY
+COMMISSION_PCT
+MANAGER_ID
+DEPARTMENT_ID
+
+
+SELECT EMPLOYEE_id first_name, last_name, salary
+FROM 
+
+
+
+
+
+
+
+
+
+2.2 조건절
+전테 데이터에서 특전 행 데이터를 제한하여 조회하기 위해서 조건절을 사용합니다
+/*
+SELECT 컬럼1, 컬럼,...               (3)
+FROM 테이블 이름                     (1)    
+WHERE 조건을 나열;                   (2)
+*/
+
+
+2-3 80번 부서원의 사원 정보를 조회하시오
+--스페이스바--
+SELECT
+FROM employees
+WHERE department_id = 80; -- 같다 연산자 : =(equal)
+--탭키-- 
+SELECT  *
+FROM    employees
+WHERE   department_id = 80;
+--ctrl+f7--
+SELECT
+    *
+FROM
+    employees
+WHERE
+    department_id = 80;
+    
+    전체부서 몇개인지 어떤부서코드를 갖고있는지 조회
+    
+DESC departments;
+SELECT *
+FROM departments; --27 rows
+ddd
+
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
